b38357df83fb476e74dc7509cafb8dd0922d03cb
gitbugd20p <155899250+gitbugd20p@users.noreply.github.com>
2026-01-02 16:07:08 +0600

    Added feature: Search (debounced)  -search is based on name, department, role, status  -debounced time is 500ms

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,10 @@
 import { Button, Space } from "antd";
 import EmployeeTable from "../components/employees/EmployeeTable";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import AddEmployeeDrawer from "../components/employees/AddEmployeeDrawer";
 import { useEmployees } from "./../hooks/useEmployees";
 import EditEmployeeDrawer from "../components/employees/EditEmployeeDrawer";
+import Search from "antd/es/transfer/search";
 
 const Employees = () => {
   // Employee info with functions
@@ -24,16 +25,63 @@ const Employees = () => {
   // Edit employee data
   const [editEmployeeInfo, setEditEmployeeInfo] = useState(null);
 
+  // Input-search text
+  const [searchText, setSearchText] = useState("");
+
+  // Input-search text for debounce search
+  const [debounceText, setDebounceText] = useState("");
+
+  // Debounce search
+  useEffect(() => {
+    const debounceTimerFunction = setTimeout(() => {
+      setDebounceText(searchText);
+    }, 500);
+
+    return () => clearTimeout(debounceTimerFunction);
+  }, [searchText]);
+
+  // Filters the main data for search and multi filters
+  const filteredEmployees = useMemo(() => {
+    return (
+      allEmployees
+        // For Search (name, department, role, status)
+        .filter((employee) => {
+          if (!debounceText) return true;
+          const text = debounceText.toLowerCase();
+
+          return (
+            employee.name.toLowerCase().includes(text) ||
+            employee.department.toLowerCase().includes(text) ||
+            employee.role.toLowerCase().includes(text) ||
+            employee.status.toLowerCase().includes(text)
+          );
+        })
+    );
+  }, [allEmployees, debounceText]);
+
   return (
     <>
       <div className="w-full">
         <h1 className="py-8 text-4xl font-bold">Employee Records</h1>
 
-        {/* Add employee button (drawer) */}
         <Space>
-          <Button onClick={() => setOpenDrawer(true)} type="primary">
-            Add Employee
-          </Button>
+          {/* Add employee button (drawer) */}
+          <Space>
+            <Button onClick={() => setOpenDrawer(true)} type="primary">
+              Add Employee
+            </Button>
+          </Space>
+
+          {/* Employee search and filters */}
+          <Space>
+            {/* Search */}
+            <Search
+              placeholder="input search text"
+              style={{ width: 200 }}
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </Space>
         </Space>
 
         {/* Add drawer */}
@@ -53,7 +101,7 @@ const Employees = () => {
 
         {/* Table */}
         <EmployeeTable
-          data={allEmployees}
+          data={filteredEmployees}
           onEdit={(employee) => {
             setOpenEditDrawer(true);
             setEditEmployeeInfo(employee);
